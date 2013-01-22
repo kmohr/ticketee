dad54b98cfea89dc4bb4c3cc74f4a8a2bb2ba87e
kellimohr <kellimohr@gmail.com>
2013-01-22 12:51:59 -0800

    Added dynamic form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,14 +8,16 @@ class ProjectsController < ApplicationController
   end
 
   def create
-  	@project = Project.new(params[:project]) #<co id="ch03_551_1"/>
-  	if @project.save
-    	flash[:notice] = "Project has been created."
-    	redirect_to @project
-  	else
-    	# nothing, yet
-  	end
-  end
+	  @project = Project.new(params[:project])
+	  if @project.save
+	    flash[:notice] = "Project has been created."
+	    redirect_to @project
+	  else
+	    flash[:alert] = "Project has not been created."
+	    #<co id="ch03_926_1"/>
+	    render :action => "new"
+	   end 
+	end
 
   def show
   	@project = Project.find(params[:id])
