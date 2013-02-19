95ee42e94085a4a69f41f8ef0e71fa3fc2444deb
kellimohr <kellimohr@gmail.com>
2013-02-19 08:56:24 -0800

    Add admin namespaced users controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,15 +26,6 @@ class ProjectsController < ApplicationController
 	redirect_to projects_path
   end
 
-  private
-    def authorize_admin!
-      authenticate_user!
-      unless current_user.admin?
-        flash[:alert] = "You must be an admin to do that."
-        redirect_to root_path
-      end
-    end
-
   def new
   	@project = Project.new
   end
