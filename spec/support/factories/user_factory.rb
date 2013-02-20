3e438eab8b8289bcf360e3d65bbea1d62aee2c79
kellimohr <kellimohr@gmail.com>
2013-02-19 17:58:13 -0800

    Fixed unique email issues.  Added Delete user link.

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -3,13 +3,16 @@ FactoryGirl.define do
     email "ticketee@email.com"
     password "password"
     password_confirmation "password"
+
     factory :confirmed_user do
+      email "confirmed_user@email.com"
       after_create do |user|
         user.confirm!
       end
 	  end
 
     factory :admin_user do
+      email "admin_user@email.com"
       after_create do |user|
         user.confirm!
         user.update_attribute(:admin, true)
