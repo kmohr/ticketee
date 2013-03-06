6224b3f7ac14ce4a43e6ba7021c7de8fa06695bd
kellimohr <kellimohr@gmail.com>
2013-03-05 18:30:58 -0800

    updated spec for expand_path to pass javascript code.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -2,7 +2,7 @@ class FilesController < ApplicationController
 	before_filter :authenticate_user!
     def show
       asset = Asset.find(params[:id])
-      if can?(:view, asset.ticket.project)
+      if can?(:view, asset.ticket.project) || current_user.admin?
         send_file asset.asset.path,
               :filename     => asset.asset_file_name,
               :content_type => asset.asset_content_type
